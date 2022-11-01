d830fcef0bb89566cdd14c2327de886f02ccbd19
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2022-11-01 17:08:18 +0100

    control project as subproject

---
+++ ar4k-brainctl/src/test/java/org/apache/flink/RunnerFlinkTests.java
@@ -0,0 +1,14 @@
+package org.apache.flink;
+
+import org.apache.flink.streaming.examples.join.WindowJoin;
+
+public class RunnerFlinkTests {
+
+	// TODO completare test Flink
+	// @Test
+	public void runWindowJoinExample() throws Exception {
+		WindowJoin.main(new String[0]);
+
+	}
+
+}
